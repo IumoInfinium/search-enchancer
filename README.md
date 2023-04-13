4093fb513c8077e35c217bd3aa3ba1eb8a0ee4f8
IumoInfinium <iumoinfinium1331@gmail.com>
2023-04-13 19:43:38 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Search-Enchancer
+
+Enchances the google search by adding a new feature to search on the youtube for the videos.
+
+## How to use
+
+1. Clone the repository.
+2. Open your browser and go to `chrome://extensions`.
+3. Enable developer tools, and then select **load unpacked**.
+4. On folder selection prompt, browse and locate your cloned repository and select it.
+5. Done
+
+Now go to [Google](https://www.google.com) and search and get the search results at both platforms.
+
---
+++ scripts/searcher.js
@@ -0,0 +1,66 @@
+// Script loaded on every request of google.com
+// What it does ?
+// Adds a button to search on youtube for the searched google text
+
+
+// adds a button to search on youtube
+// works on google results page
+const menu_bar = document.getElementById("hdtb-msb") ?? null;
+
+const searchResultsElement = document.createElement("button");
+searchResultsElement.textContent="YouTube";
+searchResultsElement.classList.add("t2vtad");
+searchResultsElement.style.backgroundColor= "transparent";
+
+// if the menu bar exists, add the button onclick event to it.
+if(menu_bar != null){
+    searchResultsElement.onclick = () =>{
+        const input_text_element = document.evaluate(
+            '/html/body/div[4]/div[2]/form/div[1]/div[1]/div[2]/div/div[2]/input',
+            document,
+            null,
+            XPathResult.FIRST_ORDERED_NODE_TYPE,
+            null,
+        ).singleNodeValue;
+        
+        let searched_text = `${input_text_element.value}`;
+        if(searched_text != ""){
+            searched_text.replace(' ', '+');
+            window.location.href= `https://youtube.com/results?search_query=${searched_text}`;
+        }
+    }
+    menu_bar.appendChild(searchResultsElement);
+}
+
+
+// adds a button to search on youtube
+// works google search homepage
+
+const searchHomepageElement = document.evaluate(
+    '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[1]/div',
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+).singleNodeValue ?? null;
+
+// add an onclick event to the search button
+if(searchHomepageElement != null){
+    searchHomepageElement.onclick = ()=> {
+        console.log("Here 1!");
+        const input_element = document.evaluate(
+            '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input',
+            document,
+            null,
+            XPathResult.FIRST_ORDERED_NODE_TYPE,
+            null,
+        ).singleNodeValue;
+    
+        let searched_text = `${input_element.value}`;
+    
+        if(searched_text != ""){
+            searched_text.replace(' ','+');
+            window.location.href= `https://youtube.com/results?search_query=${searched_text}`;
+        }
+    };
+}    
